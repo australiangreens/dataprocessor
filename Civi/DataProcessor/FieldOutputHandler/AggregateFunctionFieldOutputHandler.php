fce409eaf6660f9e2be60502ca2bb54fa56c52f4
Jaap Jansma <jaap.jansma@civicoop.org>
2020-09-09 12:11:39 +0200

    calculated field should be string

--- Civi/DataProcessor/FieldOutputHandler/AggregateFunctionFieldOutputHandler.php
+++ Civi/DataProcessor/FieldOutputHandler/AggregateFunctionFieldOutputHandler.php
@@ -56,13 +56,13 @@ class AggregateFunctionFieldOutputHandler extends AbstractSimpleFieldOutputHandl
    * @return \Civi\DataProcessor\FieldOutputHandler\FieldOutput
    */
   public function formatField($rawRecord, $formattedRecord) {
-    $value = $rawRecord[$this->aggregateField->alias];
+    $value = (float) $rawRecord[$this->aggregateField->alias];
 
     $formattedValue = $value;
     if (is_numeric($this->number_of_decimals) && $value != null) {
-      $formattedValue = number_format($value, $this->number_of_decimals, $this->decimal_sep, $this->thousand_sep);
+      $formattedValue = number_format((float) $value, $this->number_of_decimals, $this->decimal_sep, $this->thousand_sep);
     } elseif ($this->inputFieldSpec->type == 'Money') {
-      $formattedValue = \CRM_Utils_Money::format($value);
+      $formattedValue = \CRM_Utils_Money::format((float) $value);
     }
     if ($formattedValue != null) {
       $formattedValue = $this->prefix . $formattedValue . $this->suffix;
